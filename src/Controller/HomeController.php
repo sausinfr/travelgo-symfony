6b473e4a62a6c7607d129a28ceb021874458fd10
sausinfr <sebastien@sausin.fr>
2023-12-01 23:50:50 +0100

    Ajout des derniers articles sur la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(PostRepository $repo): Response
     {
-        return $this->render('home/index.html.twig');
+        $lastPosts = $repo->lastPosts();
+        return $this->render('home/index.html.twig', [
+            'lastPosts' => $lastPosts
+        ]);
     }
 }
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -24,17 +24,15 @@ class PostRepository extends ServiceEntityRepository
 //    /**
 //     * @return Post[] Returns an array of Post objects
 //     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+   public function lastPosts(): array
+   {
+       return $this->createQueryBuilder('p')
+           ->orderBy('p.id', 'DESC')
+           ->setMaxResults(4)
+           ->getQuery()
+           ->getResult()
+       ;
+   }
 
 //    public function findOneBySomeField($value): ?Post
 //    {
